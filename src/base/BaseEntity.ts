9b934170908254eed828719d50deb0f2e6fecb8c
Hamm <majhamm@qq.com>
2023-06-28 18:17:27 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/base/BaseEntity.ts
+++ src/base/BaseEntity.ts
@@ -15,26 +15,23 @@ import { AirEntity } from '@/airpower/base/AirEntity'
   keywordSearchPlaceholder: '搜的一下~',
 })
 export class BaseEntity extends AirEntity {
-  @FieldName('创建时间')
   @TableField({
     width: 170,
     orderNumber: -99,
     dateTimeFormatter: AirDateTimeFormatter.YYYY_MM_DD_HH_mm_ss,
   })
-    createTime!: number
+  @FieldName('创建时间') createTime!: number
 
-  @FieldName('状态')
   @TableField({
     enumRecord: DisableRecord,
     showStatus: true,
     width: 80,
     orderNumber: -100,
   })
-    isDisabled!: boolean
+  @FieldName('状态') isDisabled!: boolean
 
-  @FieldName('备注')
   @FormField({
     isTextarea: true,
   })
-    remark!: string
+  @FieldName('备注') remark!: string
 }
