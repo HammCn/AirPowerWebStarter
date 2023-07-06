05647101ffa8cbee8ba9a05f4f0f6a9ff21ab959
Hamm <majhamm@qq.com>
2023-07-06 10:18:44 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/base/AbstractBaseService.ts
+++ src/base/AbstractBaseService.ts
@@ -1,12 +1,12 @@
-import { AirAbstractService } from '@/airpower/base/AirAbstractService'
 import { BaseEntity } from './BaseEntity'
 import { AirHttp } from '@/airpower/helper/AirHttp'
+import { AirAbstractEntityService } from '@/airpower/base/AirAbstractEntityService'
 
 /**
  * # 抽象服务基类
  * @author Hamm
  */
-export abstract class AbstractBaseService<E extends BaseEntity> extends AirAbstractService<E> {
+export abstract class AbstractBaseService<E extends BaseEntity> extends AirAbstractEntityService<E> {
   api(url: string, baseUrl?: string | undefined): AirHttp {
     return super.api(url, baseUrl)
       .addHttpHeader('hello', 'world')
