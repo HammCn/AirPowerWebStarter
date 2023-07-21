c0d6b011def56a5696f8469cf55ec5253c8fb05d
Hamm <majhamm@qq.com>
2023-07-21 14:57:29 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,7 @@ import { AirConfig } from '@/airpower/config/AirConfig'
 
 import { routes } from '@/config/routes'
 import '@/assets/css/main.scss'
-import { MaterialEntity } from './model/entity/MaterialEntity'
 
 AirConfig.successCode = 200000
 AirConfig.autoPermission = false
 app.use(AirConfig.createRouter(routes)).mount('#app')
-
-const material = new MaterialEntity()
-// eslint-disable-next-line no-console
-console.log(material)
-
-// eslint-disable-next-line no-console
-console.log(MaterialEntity.getTableFieldConfigList())
