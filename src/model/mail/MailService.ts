14cd265b84caa96c2d729c6144344abcf2c9e623
Hamm <majhamm@qq.com>
2023-08-01 18:08:22 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/model/mail/MailService.ts
+++ src/model/mail/MailService.ts
@@ -1,21 +1,14 @@
 import { AirAbstractService } from '@/airpower/base/AirAbstractService'
 import { UserEntity } from '../user/UserEntity'
-import { AirHttp } from '@/airpower/helper/AirHttp'
 
 export class MailService extends AirAbstractService {
   baseUrl = 'mail'
-
-  api(url: string, baseUrl?: string | undefined): AirHttp {
-    const http = super.api(url, baseUrl)
-    http.setTimeout(10000)
-    return http
-  }
 
   /**
    * # 发送验证码
    * @param user 用户
    */
   async sendCode(user: UserEntity): Promise<void> {
-    await this.api('send').post(user)
+    await this.api('send').setTimeout(10000).post(user)
   }
 }
