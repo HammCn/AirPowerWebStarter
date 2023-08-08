075bedbd1f22f49eb898ff0ded5983f2c842a39a
Hamm <majhamm@qq.com>
2023-08-08 18:14:39 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,8 @@ import { AirConfig } from '@/airpower/config/AirConfig'
 
 import { routes } from '@/config/routes'
 import '@/assets/css/main.scss'
-import { AirDialog } from './airpower/helper/AirDialog'
-import { RoleSelector } from './view/console/role/component'
 
 AirConfig.successCode = 200000
 AirConfig.unAuthorizeCode = 401000
 AirConfig.autoPermission = true
 app.use(AirConfig.createRouter(routes)).mount('#app')
-
-async function test() {
-  await AirDialog.select(RoleSelector)
-  await AirDialog.selectList(RoleSelector)
-}
-
-test()
