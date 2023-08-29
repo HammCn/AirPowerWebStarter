ac1959ab30a758424027e8e0eccdd7e343346c89
Hamm <majhamm@qq.com>
2023-08-29 09:41:24 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/model/menu/MenuService.ts
+++ src/model/menu/MenuService.ts
@@ -2,7 +2,7 @@ import { AbstractBaseService } from '@/base/AbstractBaseService'
 import { MenuEntity } from './MenuEntity'
 
 /**
- * # 权限接口服务
+ * # 菜单接口服务
  * @author Hamm
  */
 export class MenuService extends AbstractBaseService<MenuEntity> {
