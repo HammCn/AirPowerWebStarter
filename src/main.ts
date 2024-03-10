105977c1bbad6e6a489d1488cb816f62ed0e83d7
Hamm <majhamm@qq.com>
2024-03-10 14:40:34 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,4 @@ import { AirConfig } from '@/airpower/config/AirConfig'
 import { routes } from '@/config/routes'
 import '@/assets/css/main.scss'
 
-AirConfig.successCode = 200000
-AirConfig.unAuthorizeCode = 401000
 app.use(AirConfig.createRouter(routes)).mount('#app')
