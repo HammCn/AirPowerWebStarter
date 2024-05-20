6d2eecf953e3d028436e03298a8b605662d66772
Hamm <majhamm@qq.com>
2024-05-20 17:03:07 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import { AirI18n } from './airpower/helper/AirI18n'
-import { ChineseSimplified } from './config/i18n/ChineseSimplified'
-import { English } from './config/i18n/English'
-
 import { app } from '@/airpower'
 import { AirConfig } from '@/airpower/config/AirConfig'
+import { AirI18n } from '@/airpower/helper/AirI18n'
+import { ChineseSimplified } from '@/config/i18n/ChineseSimplified'
+import { English } from '@/config/i18n/English'
 
 import { createRoutes } from '@/config/routes'
 import '@/assets/css/main.scss'
-import { AppConfig } from './config/AppConfig'
-import { UserEntity } from './model/user/UserEntity'
+import { AppConfig } from '@/config/AppConfig'
+import { UserEntity } from '@/model/user/UserEntity'
 
 AirI18n.init(ChineseSimplified, English)
 
