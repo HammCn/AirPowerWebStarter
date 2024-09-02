6372c184d06f7ffecb3b7419b10c497d0071a7e8
Hamm <majhamm@qq.com>
2024-09-02 17:00:51 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/view/console/index/index.vue
+++ src/view/console/index/index.vue
@@ -1,12 +1,29 @@
 <template>
   <APanel>
-    <AEmpty>Hi guys</AEmpty>
+    <div class="index">
+      <iframe src="https://ac.hamm.cn" />
+    </div>
   </APanel>
 </template>
 
 <script lang="ts" setup>
-import {
-  APanel, AEmpty,
-} from '@/airpower/component'
-
+import { APanel } from '@/airpower/component'
 </script>
+
+<style scoped lang="scss">
+.index{
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  iframe{
+    width: 400px;
+    height: auto;
+    border:none;
+  }
+}
+</style>
