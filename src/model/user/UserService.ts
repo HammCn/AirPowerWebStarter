4ac352f3e977a6db8085794e7c237ca48020256a
Hamm <majhamm@qq.com>
2024-11-26 15:59:45 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/model/user/UserService.ts
+++ src/model/user/UserService.ts
@@ -11,58 +11,58 @@ export class UserService extends AbstractBaseService<UserEntity> {
   entityClass = UserEntity
 
   /**
-     * # App登录
-     * @param user 用户
-     */
+   * # App登录
+   * @param user 用户
+   */
   async login(user: UserEntity): Promise<string> {
     const result = await this.api('login').post(user)
     return result as unknown as string
   }
 
   /**
-     * # 邮箱验证码登录
-     * @param user 用户
-     */
+   * # 邮箱验证码登录
+   * @param user 用户
+   */
   async loginViaEmail(user: UserEntity): Promise<string> {
     const result = await this.api('loginViaEmail').post(user)
     return result as unknown as string
   }
 
   /**
-     * # 注册
-     * @param user 用户
-     */
+   * # 注册
+   * @param user 用户
+   */
   async register(user: UserEntity): Promise<void> {
     await this.api('register').post(user)
   }
 
   /**
-     * # 重置密码
-     * @param user 用户
-     */
+   * # 重置密码
+   * @param user 用户
+   */
   async resetMyPassword(user: UserEntity): Promise<void> {
     await this.api('resetMyPassword').post(user)
   }
 
   /**
-     * # 获取我有权限的菜单列表
-     */
+   * # 获取我有权限的菜单列表
+   */
   async getMyMenuList(): Promise<MenuEntity[]> {
     const jsonArray = await this.api('getMyMenuList').post()
     return MenuEntity.fromJsonArray(jsonArray)
   }
 
   /**
-     * # 获取我d的权限列表
-     */
+   * # 获取我d的权限列表
+   */
   async getMyPermissionList(): Promise<string[]> {
     const arr = await this.api('getMyPermissionList').post()
     return arr as string[]
   }
 
   /**
-     * # 获取我的个人信息
-     */
+   * # 获取我的个人信息
+   */
   async getMyInfo(): Promise<UserEntity> {
     const json = await this.api('getMyInfo').post()
     return UserEntity.fromJson(json)
