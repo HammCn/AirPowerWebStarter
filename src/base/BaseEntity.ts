f52320d2bb5129a3d570b0a76e3e9d0cabf6d9c0
Hamm <majhamm@qq.com>
2024-12-05 00:18:15 +0800

    refactor(base): 移除 BaseEntity 中的 remark 字段
    - 删除了 BaseEntity 类中的 remark 属性
    - 移除了与 remark 相关的 Search、Form 和 Field装饰器

--- src/base/BaseEntity.ts
+++ src/base/BaseEntity.ts
@@ -1,10 +1,7 @@
-import { Form } from '@/airpower/decorator/FormField'
 import { Table } from '@/airpower/decorator/TableField'
 import { AirDateTimeFormatter } from '@/airpower/enum/AirDateTimeFormatter'
 import { Model } from '@/airpower/decorator/Model'
 import { AirEntity } from '@/airpower/base/AirEntity'
-import { Search } from '@/airpower/decorator/SearchField'
-import { AirSearchDataType } from '@/airpower/enum/AirSearchDataType'
 import { Field } from '@/airpower/decorator/Field'
 
 /**
@@ -24,19 +21,4 @@ export class BaseEntity extends AirEntity {
     type: Number,
   })
     createTime!: number
-
-  @Search({
-    orderNumber: -100,
-    dataType: AirSearchDataType.TEXTAREA,
-    hide: true,
-  })
-  @Form({
-    textarea: true,
-    orderNumber: -100,
-    hide: true,
-  })
-  @Field({
-    label: '备注',
-  })
-    remark!: string
 }
